ef4c5eaafb8f44a27c0285f9afcf932a82314285
gaelleponts <105050115+gaelleponts@users.noreply.github.com>
2022-05-19 13:44:03 +0200

    Update industrial_agent.py

--- microgrid/agents/industrial_agent.py
+++ microgrid/agents/industrial_agent.py
@@ -6,13 +6,54 @@ class IndustrialAgent:
     def __init__(self, env: IndustrialEnv):
         self.env = env
 
-    def take_decision(self,
+        def take_decision(self,
                       state,
                       previous_state=None,
                       previous_action=None,
                       previous_reward=None):
-        return self.env.action_space.sample()
 
+        #données utiles
+        delta_t = datetime.timedelta(minutes=30)  #pas de temps
+        T=self.env.nb_pdt #cf industrial_env.py  nb de periode temporelles
+        capacity= self.env.battery.capacity  #cf battery.py charge max
+        pmax=self.env.battery.pmax #cf battery.py puissance max
+        efficiency=self.env.battery.efficiency #cf battery.py rendement de la batterie
+
+        manager_signal= state.get("manager_signal") #prix  cf manager.py l95
+        consumption_prevision = state.get("consumption_prevision")  # la demande de consommation
+        H = datetime.timedelta(hours=1)
+
+        #problème:
+        pb=pulp.LpProblem("Site industriel", pulp.LpMinimize)
+
+        #variabless
+        l_bat_pos = pulp.LpVariable.dicts("l_bat_pos", [t for t in range(T)], 0)  #l_bat+
+        l_bat_neg = pulp.LpVariable.dicts("l_bat_neg", [t for t in range(T)], 0)  #l_bat-
+        l_bat = pulp.LpVariable.dicts("l_bat", [t for t in range(T)])
+        li = pulp.LpVariable.dicts("li", [t for t in range(T)])  #demande totale du site industriel
+        a = pulp.LpVariable.dicts("stock_batterie", [t for t in range(T)], 0, capacity) #cf formulation mathématique  0 <= a <= C
+
+        #fonction objectifll
+
+        pb += pulp.lpSum([li[t] * manager_signal[t] * delta_t/H  for t in range(T)])
+
+
+        #contraintes
+        pb += a[0]==0
+        pb += pulp.lpSum([l_bat_pos[t] for t in range(T)]) <= pmax
+        pb += pulp.lpSum([l_bat_neg[t] for t in range(T)]) <= pmax
+        pb += pulp.lpSum([li[t] - consumption_prevision[t] - l_bat[t] for t in range(T)])== 0  #li(t)=ldem(t)+lbat(t) cf formulation math
+        pb += pulp.lpSum([a[t]-a[t-1]- (efficiency*l_bat_pos[t] - l_bat_neg[t]*1/efficiency)*delta_t/H for t in range(1,T)])==0  #formule de recurrence a(t)
+
+
+        #Résolution
+        pb.solve()
+
+        resultat = self.env.action_space.sample()
+        for t in range(T):
+            resultat[t] = li[t].value()
+
+        return resultat
 
 if __name__ == "__main__":
     delta_t = datetime.timedelta(minutes=30)
